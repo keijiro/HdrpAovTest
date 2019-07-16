ff4ae52246078646fa8e49d61cd67b3bd91f23bb
Keijiro Takahashi <keijiro@gmail.com>
2019-07-16 19:17:35 +0900

    Initial commit

---
+++ Assets/AovTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Experimental.Rendering.HDPipeline;
+
+class AovTest : MonoBehaviour
+{
+    [SerializeField] RenderTexture _output = null;
+
+    RTHandleSystem.RTHandle _rtHandle;
+
+    void Start()
+    {
+        _rtHandle = RTHandles.Alloc(_output.width, _output.height);
+
+        var request = new AOVRequest(AOVRequest.@default);
+        request.SetFullscreenOutput(DebugFullScreen.Depth);
+
+        GetComponent<HDAdditionalCameraData>().SetAOVRequests(
+            new AOVRequestBuilder().Add(
+                request,
+                bufferID => _rtHandle,
+                null,
+                new [] { AOVBuffers.DepthStencil },
+                (cmd, textures, properties) => { cmd.Blit(textures[0], _output); }
+            ).Build()
+        );
+    }
+}
